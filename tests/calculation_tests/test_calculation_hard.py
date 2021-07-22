280ff6d47af0408a1664524969c0665fbf061db3
Alexey Khorkin <alexeykhorkin12@gmail.com>
2021-07-22 19:15:40 +0300

    add dataclass test cases

--- tests/calculation_tests/test_calculation_hard.py
+++ tests/calculation_tests/test_calculation_hard.py
@@ -1,16 +1,15 @@
 import pytest
 from calculator.parser import parse_operation
 from calculator.tokenizer import Tokenizer
+from ..tests_utities import CaseCalc, EPS
 
-EPS = 1e-3
-
-HARD_CASES = [("7*(-1+2*(3*2*4+15*431)*1+((2*3)*11+314))", 93499),
-              ("19 + 2 / 3 + (1 + 1 + 394) / 7 / 2", 47.94),
-              ("1 * 2 * 3 * 4 * 5 * 6 * 7 * 8 * 9 * 10", 3628800)]
+HARD_CASES = [CaseCalc("7*(-1+2*(3*2*4+15*431)*1+((2*3)*11+314))", 93499),
+              CaseCalc("19 + 2 / 3 + (1 + 1 + 394) / 7 / 2", 47.94),
+              CaseCalc("1 * 2 * 3 * 4 * 5 * 6 * 7 * 8 * 9 * 10", 3628800)]
 
 
-@pytest.mark.parametrize("input, expected_res", HARD_CASES)
-def test_hard_caces(input, expected_res) -> None:
-    iterator = Tokenizer(input).__iter__()
+@pytest.mark.parametrize("case", HARD_CASES)
+def test_easy_caces_calc(case: CaseCalc) -> None:
+    iterator = Tokenizer(case.given).__iter__()
     op = parse_operation(iterator)
-    assert op.execute() == pytest.approx(expected_res, EPS)
+    assert op.execute() == pytest.approx(case.expected, EPS)
--- tests/tokenizer_test/test_tokenizer.py
+++ tests/tokenizer_test/test_tokenizer.py
@@ -1,20 +1,22 @@
 import pytest
 import calculator.tokens as tks
 from calculator.tokenizer import Tokenizer
-
-CASES = [("1", [tks.ConstantToken]),
-         ("1 + 2", [tks.ConstantToken,
-                    tks.SumToken,
-                    tks.ConstantToken]),
-         ("(2*3)", [tks.OpenBracketToken,
-                    tks.ConstantToken,
-                    tks.MulToken,
-                    tks.ConstantToken,
-                    tks.CloseBracketToken])]
+from ..tests_utities import CaseTokenizer
 
 
-@pytest.mark.parametrize("input, expected_tokens", CASES)
-def test_types_of_tokens(input, expected_tokens) -> None:
-    for idx, tok in enumerate(Tokenizer(input)):
-        assert isinstance(tok, expected_tokens[idx])
-    assert idx + 1 == len(expected_tokens)
+CASES = [CaseTokenizer("1", [tks.ConstantToken]),
+         CaseTokenizer("1 + 2", [tks.ConstantToken,
+                                 tks.SumToken,
+                                 tks.ConstantToken]),
+         CaseTokenizer("(2*3)", [tks.OpenBracketToken,
+                                 tks.ConstantToken,
+                                 tks.MulToken,
+                                 tks.ConstantToken,
+                                 tks.CloseBracketToken])]
+
+
+@pytest.mark.parametrize("case", CASES)
+def test_types_of_tokens(case: CaseTokenizer) -> None:
+    for idx, tok in enumerate(Tokenizer(case.given)):
+        assert isinstance(tok, case.expected[idx])
+    assert idx + 1 == len(case.expected)
