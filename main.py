f7514c0efb81b0c9202e5a81f2cb95f17be8390d
Alexey Khorkin <alexeykhorkin12@gmail.com>
2021-07-27 02:33:07 +0300

    add repl

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
-from calculator import tokenizer
-from calculator.parser import parse_operation
+from calculator import calc
+
+
+def main():
+    print("Press 'q' for quit")
+    while True:
+        try:
+            input_str_for_calc = input(">>> ")
+            if input_str_for_calc == 'q':
+                break
+            res = calc(input_str_for_calc)
+            print(res)
+        except Exception as err:
+            print(f"An exception was found: {err}")
+
 
 if __name__ == "__main__":
-    inp: str = "1 / 2"
-    iterator = tokenizer.Tokenizer(inp).__iter__()
-    op = parse_operation(iterator)
-    print(op.execute())
+    main()
